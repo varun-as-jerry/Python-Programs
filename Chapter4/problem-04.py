1dcbd5476ad5aa98e4de7a525736e38da1ddf426
varun <pawarvarun706@gmail.com>
2023-01-24 22:49:29 +0530

    initial commit

---
+++ Chapter4/problem-04.py
@@ -0,0 +1,12 @@
+
+# Questions: write a program to sum a list of four numbers.
+
+list = [8,9,7,9]
+
+print(list[0]+list[1]+list[2]+list[3]) # first method 
+
+print(sum(list)) # second method
+
+
+
+
---
+++ Chapter4/problem_05.py
@@ -0,0 +1,8 @@
+# write a program to count the number of zeroes in the following tuple.
+
+a = (7,0,8,0,0,9)
+
+print(a.count(0))
+
+
+# it was a little bit tricky but done.
